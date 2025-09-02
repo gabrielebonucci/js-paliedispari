d9f1d76dcb0f052d3ca6c5c6f91096cc7dd9d226
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-02 16:09:34 +0200

    passaggi logici "Pari e Dispari"

---
+++ pari e dispari/script.js
@@ -0,0 +1,9 @@
+//PASSAGGI LOGICI
+
+// Chiedo all'utente di scegliere pari o dispari.
+// Chiedo all'utente un numero da 1 a 5.
+// Genero un numero casuale da 1 a 5 per il computer.
+// Sommo il numero dell'utente e quello del computer.
+// Controllo se la somma è un numero pari o dispari.
+// Confronto il risultato con la scelta iniziale per determinare il vincitore.
+// Comunico il vincitore e tutti i dati della partita.
