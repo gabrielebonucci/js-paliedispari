10ee0d020dc6e7d2b29b530d3dfc87be2a6a78b9
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-02 15:29:06 +0200

    passaggi logici

---
+++ palindroma/script.js
@@ -0,0 +1,9 @@
+
+//PASSAGGI LOGICI
+
+//Chiedo all'utente di inserire una parola.
+//Creo una funzione che prenderà questa parola come input.
+//Dentro la funzione, inverto la parola.
+//Confronto la parola originale con quella invertita.
+//la funnzione mi deve dire se sono uguali o no.
+//in base al risultato comunico se la parola è palindroma o meno.
